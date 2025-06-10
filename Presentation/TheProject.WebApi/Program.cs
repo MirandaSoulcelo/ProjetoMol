cf3590fc9f837549e5bde2791d02106b757b68b3
Miranda Soulcelo <marcelofmiranda23@gmail.com>
2025-06-10 20:10:34 -0300

    adicionando pactoe nuget para utilizar token e iniciando implementação e configuração do midllewar token JWt

--- Presentation/TheProject.WebApi/Program.cs
+++ Presentation/TheProject.WebApi/Program.cs
@@ -1,5 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Components;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
+
+
+var builder = WebApplication.CreateBuilder(args);
+//Aqui estou definindo a chave 'secreta' para assinar o token
+var key = Encoding.ASCII.GetBytes("123");
+
+
+
+builder.Services.AddAuthentication(options =>
+{
+    //definindo qual esquema de autenticação que o meu sistema vai usar como padrão
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    //caso um user tentar acessar sem estar autenticado, vai levar um belo de um 404
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{   //configuração do middleware JWT Bearer, Marcelo. Não esquecer disso
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        //o sistema vai ter que validar se esse carinha(token) foi de fato assinado O.o
+        ValidateIssuerSigningKey = true,
+
+        //passando minha chave nada secreta para ser usada para validar a assinatura
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+
+        //aqui eu não to validando quem emitiu o token, pelo que entendi
+        ValidateIssuer = false,
+
+        // nem o público
+        ValidateAudience = false,
+        //sem tolerância, igual a igreja católica no período da inquisição
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
