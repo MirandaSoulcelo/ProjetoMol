8478d8c7d835d34767af40e414d04072112d83d3
Miranda Soulcelo <marcelofmiranda23@gmail.com>
2025-06-10 20:45:14 -0300

    iniciando implementação de login

--- Presentation/TheProject.WebApi/Services/TokenService.cs
+++ Presentation/TheProject.WebApi/Services/TokenService.cs
@@ -1,8 +1,8 @@
 using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt; //fornece a funcionalidade de criar e manipular tokens JWT
+using System.Security.Claims; // permite trabalhar com "claims" (informações embutidas no token), não posso esquecer.
 using System.Text;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens; // define estruturas e classes para criptografia e segurança dos tokens.
 
 public class TokenService
 {
@@ -16,11 +16,16 @@ public class TokenService
 
     public string GetToken(string nome, string email)
     {
+
+        //Cria um manipulador de tokens. Ele que vai criar e serializar (converter para string) o token.
         var tokenHandler = new JwtSecurityTokenHandler();
+
+        //aqui converte para um array de bytes
         var key = Encoding.UTF8.GetBytes(_secretKey); ;
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            //aqui eu to definindo como o token será criado
             Subject = new ClaimsIdentity(new Claim[]
             {
                 new Claim("user_name", nome),
@@ -29,11 +34,15 @@ public class TokenService
             }),
 
             Expires = DateTime.UtcNow.AddMinutes(15),
+
+            // define o algoritmo de assinatura do token (HMAC-SHA256) e a chave secreta.
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
 
-
+        //criando uum token com base no descriptor
         var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        //se não me engano WriteToken serve pra converter pra string
         return tokenHandler.WriteToken(token);
     }
  }
